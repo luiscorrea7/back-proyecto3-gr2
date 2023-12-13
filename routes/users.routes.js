11ce5692d01a0de4ede0686de13329bdc0298663
Emmanuel Aguirre <manu93_47@hotmail.com>
2023-12-13 17:15:13 -0300

    creando rutas protegidas

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,38 +1,53 @@
-const { Router } = require('express');
-const { getAllUsers, getUserById, getUserByEmail, createUser, editUser, deleteUser } = require('../controllers/users.controllers');
-const { createUserValidations, mongoIdValidator, emailQueryValidator } = require('../helpers/validations');
+const { Router } = require("express");
+const {
+  getAllUsers,
+  getUserById,
+  getUserByEmail,
+  createUser,
+  editUser,
+  deleteUser,
+} = require("../controllers/users.controllers");
+const {
+  createUserValidations,
+  mongoIdValidator,
+  emailQueryValidator,
+} = require("../helpers/validations");
+const { authTokenValidation, validateRole } = require("../helpers/auth");
 const route = Router();
 
-route.get('/', getAllUsers);
+route.get("/", getAllUsers);
 
 route.get(
-  '/findByID/:id',
+  "/findByID/:id",
   [mongoIdValidator.id],
+  authTokenValidation,
   getUserById
 );
 
-route.get(
-  '/findByEmail',
-  [emailQueryValidator.email],
-  getUserByEmail
-);
+route.get("/findByEmail", [emailQueryValidator.email], getUserByEmail);
 
 route.post(
-  '/createUser', 
+  "/createUser",
   [createUserValidations.email],
   [createUserValidations.password],
+  authTokenValidation,
+  validateRole,
   createUser
 );
 
 route.patch(
-  '/editUser/:id',
+  "/editUser/:id",
   [mongoIdValidator.id],
+  authTokenValidation,
+  validateRole,
   editUser
 );
 
 route.delete(
-  '/deleteUser/:id',
+  "/deleteUser/:id",
   [mongoIdValidator.id],
+  authTokenValidation,
+  validateRole,
   deleteUser
 );
 
